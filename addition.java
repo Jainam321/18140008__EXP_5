e9d8b3c8790fb972d1cec3554bb7d19f0f41af8d
Jainam <jainam.tm@somaiya.edu>
2021-09-24 15:57:31 +0530

    added addition program

---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition {
+  public static void main(String[] args) {
+    int a,b;
+    a = Integer.parseInt(args[0]);
+    b = Integer.parseInt(args[1]);
+    System.out.println("Addition of the two numbers is " +(a+b));
+  }
+}
